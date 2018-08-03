daa845561a1a3da0c0e8c138f1a06dd0ceb60c1f
srinathshiv <srinathshiv94@gmail.com>
2018-08-02 18:47:58 -0700

    wait_queue added to queue threads to put them to sleep. Have to add synchronization

--- barrierDriver.c
+++ barrierDriver.c
@@ -1,25 +1,29 @@
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/wait.h>
 #include <linux/list.h>
 #include <linux/errno.h>
 
 static LIST_HEAD(linkedlist);
+int waitq_flag;
 
 struct barrier_content{
     struct list_head linkedlist_head;
    
-    int count;
-    int remaining_threads;
+    int count;                  // number of threads already on the barrier
+    int remaining_threads;      // number of threads that needs to hit the barrier
     int barrier_id;
     pid_t process_id;
 
     wait_queue_head_t my_q;
+    spinlock_t my_spin;
 };
 typedef struct barrier_content barrier_content;
 
 asmlinkage int barrier_init(int count, int * barrier_id){
     barrier_content *bc_ptr, *bc_temp;          //bc stands for barrier content
-   
+    int init_flag=0;  
+ 
     bc_ptr = kmalloc(sizeof(barrier_content), GFP_KERNEL );
     if( !bc_ptr) printf("\nError: Kmalloc() ");
     memset(bc_ptr, 0, sizeof(barrier_content));    
@@ -27,10 +31,17 @@ asmlinkage int barrier_init(int count, int * barrier_id){
     if( !list_empty(linkedlist) ){
     //list_empty() is valid when the linkedlist is empty. Hence this block executes when the
     //linkedlist is not empty
+    
+        if(init_flag==0)                // For the first time, we don't have to iterate the list.
+            bc_ptr -> barrier_id = 1;
+
         list_for_each_entry( bc_temp, &linkedlist, linkedlist_head){
-            if( bc_temp -> process_id == task_tgid_vnr(current) )
+            if( bc_temp -> process_id == task_tgid_vnr(current) ){
+                init_flag=1;
                 bc_ptr -> barrier_id = bc_temp->barrier_id +1;
+            }
         }
+    
     }
         
     else{ 
@@ -41,7 +52,8 @@ asmlinkage int barrier_init(int count, int * barrier_id){
         bc_ptr -> count  = 0;
     
         init_waitqueue_head( &(bc_ptr->my_q));
-        
+        spin_lock_init( &(bc_ptr->my_spin));       
+
         INIT_LIST_HEAD( barrier_content_ptr -> barrier_head);
         list_add( &bc_ptr->linkedlist_head, &linkedlist);  
         
@@ -62,17 +74,21 @@ asmlinkage int barrier_wait(int barrier_id){
             break;
         }   
     }
+    if(flag==0)
+        return -EINVAL;
 
     if( bc_temp-> count < bc_temp->remaining_threads ){
-        bc_temp -> count ++;
+        bc_temp -> count ++;        
+        wait_event_interruptible( &bc_temp->my_q, waitq_flag);
 
+        waitq_flag=0;
     }
     
     else{
-        bc_temp -> count = 0;
-        
+        waitq_flag = 1;
+        wake_up_all( &bc_temp->my_q);
+        bc_temp -> count = 0;        
     }
-
 
 return 0;
 }
