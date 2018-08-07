73ceaf6da4a3eb1908c82bdb04ba66c1db4ffa15
srinathshiv <srinathshiv94@gmail.com>
2018-08-06 17:32:39 -0700

    test program partially completed

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <errno.h>
 #include <pthreads.h>
 
+#define ITERATIONS 100
+
 struct process_args{
     int i;
     unsigned int barrier_id;
@@ -14,90 +16,96 @@ typedef struct process_args process_args;
 void childProcess();
 
 int main(int argc, char **argv){
+    pid_t pid0,pid1;
 
-pid_t pid0,pid1;
+    pid0 = fork();
+    if(pid0<0)
+        printf("\nFork Error for zeroth child");
 
-pid0 = fork();
-if(pid0<0)
-    printf("\nFork Error for zeroth child");
-
-else if(pid0==0)
-    childProcess();
-
-else if(pid>0){
-
-    pid1 = fork();
-    if(pid1<0)
-        printf("\nFork Error for first child");
-    else if(pid==0)
+    else if(pid0==0)
         childProcess();
-    
-    else if(pid>0)
-        wait(NULL);
 
+    else if(pid>0){
+        pid1 = fork();
+        if(pid1<0)
+            printf("\nFork Error for first child");
+        else if(pid==0)
+            childProcess(); 
+        else if(pid>0)
+            wait(NULL);
     wait(NULL);
-}
+    }
 
 return 0;
 }
 
 void childProcess(){
 
-unsigned int *barrier_id_set1, *barrier_id_set2;
-barrier_id_set1 = malloc(sizeof(unsigned int));
-barrier_id_set2 = malloc(sizeof(unsigned int));
-unsigned int bid1, bid2;
-int i;
+    unsigned int *barrier_id_set1, *barrier_id_set2;
+    barrier_id_set1 = malloc(sizeof(unsigned int));
+    barrier_id_set2 = malloc(sizeof(unsigned int));
+    unsigned int bid1, bid2;
+    int i;
 
-pthread_t thread_set1[5], thread_set2[20];
-process_args *args = (process_args *)malloc(sizeof(process_args));
+    pthread_t thread_set1[5], thread_set2[20];
+    process_args *args = (process_args *)malloc(sizeof(process_args));
 
-syscall( ,5, barrier_id_set1);
-bid1 = *barrier_id_set1;
+// barrier_init(unisigned int count, unsigned int *barried_id)    
+    syscall( ,5, barrier_id_set1);
+    bid1 = *barrier_id_set1;
 
-for(i=0; i<5; i++){
-    args -> i = i;
-    args -> barrier_id = bid1;
-    pthread_create( &thread_set1[i], NULL, process1, (void *)args);
+    for(i=0; i<5; i++){
+        args -> i = i;
+        args -> barrier_id = bid1;
+        pthread_create( &thread_set1[i], NULL, process1, (void *)args);
+    }
+
+// barrier_init(unsigned int count, unsigned int *barrier_id)
+    syscall( ,20, barrier_id_set2);
+    bid2 = *barrier_id_set2;
+
+    for(i=0; i<20; i++){
+        args -> i = i;
+        args -> barrier_id = bid2;
+        pthread_create( &thread_set1[i], NULL, process2, (void *)args);
+    }
+
+    for(i=0; i<5; i++)
+        pthread_join(thread_set1[i], NULL);
+
+    for(i=0; i<20; i++)
+         pthread_join(thread_set2[i], NULL);
+
+//barrier_destroy(unsigned int barrier_id)
+    syscall( ,barrier_id_set1);
+    sleep(1);
+//barrier_destroy(unsigned int barrier_id)
+    syscall( ,barrier_id_set2);
 }
 
-syscall( ,20, barrier_id_set2);
-bid2 = *barrier_id_set2;
+void * process1(void *pargs){   
+    process_args *args = (process_args *)malloc(sizeof(process_args));
+   
+    for(int i=0; i< ITERATIONS; i++){
+        args = (process_args *)pargs;
+//barrier_wait(unsigned int barrier_id)
+        syscall( , args-> barrier_id);
 
-for(i=0; i<20; i++){
-    args -> i = i;
-    args -> barrier_id = bid2;
-    pthread_create( &thread_set1[i], NULL, process2, (void *)args);
-}
-
-for(i=0; i<5; i++)
-    pthread_join(thread_set1[i], NULL);
-
-for(i=0; i<20; i++)
-    pthread_join(thread_set2[i], NULL);
-
-
-
-syscall( ,barrier_id_set1);
-syscall( ,barrier_id_set2);
-}
-
-
-void * process1(void *pargs){
-
-
-// barrier_wait
-
+    }
+    pthread_exit(0);
 }
 
 
 void * process2(void *pargs){
+    process_args *args = (process_args *)malloc(sizeof(process_args));
+   
+    for(int i=0; i< ITERATIONS; i++){
+        args = (process_args *)pargs;
+//barrier_wait(unsigned int barrier_id)
+       syscall( , args->barrier_id);
 
-//barrier_wait
-
+    }
+    pthread_exit(0);
 }
 
 
-
-
-}
